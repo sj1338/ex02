9ad75214026405676f60f3f8f2f26e4a394b0332
sj1338 <id177@naver.com>
2021-01-25 09:36:40 +0900

    a

--- lecture.sql
+++ lecture.sql
@@ -52,3 +52,17 @@ WHERE rn BETWEEN ? and ?;
 상품 이럼, 번호ㅡ 이미지 경로
 
 
+CREATE TABLE tbl_reply (
+    rno NUMBER(10, 0),
+    bno NUMBER(10, 0) NOT NULL,
+    reply VARCHAR2(1000) NOT NULL,
+    replyer VARCHAR2(50) NOT NULL,
+    replyDate DATE DEFAULT SYSDATE,
+    updateDate DATE DEFAULT SYSDATE
+    );
+
+CREATE SEQUENCE seq_reply;
+
+ALTER TABLE tbl_reply ADD CONSTRAINT pk_reply PRIMARY KEY(rno);
+
+ALTER TABLE tbl_reply ADD CONSTRAINT fk_reply_board FOREIGN KEY (bno) REFERENCES tbl_board(bno);
