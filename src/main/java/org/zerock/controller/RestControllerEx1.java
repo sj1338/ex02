05ed4686967314f73c698af887233931edcaf6de
sj1338 <id177@naver.com>
2021-01-26 15:43:00 +0900

    A

--- src/main/java/org/zerock/controller/RestControllerEx1.java
+++ src/main/java/org/zerock/controller/RestControllerEx1.java
@@ -10,7 +10,7 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/rest1")
 @Log4j
 public class RestControllerEx1 {
-	
+
 	@RequestMapping("/ex1")
 	@ResponseBody
 	public String method1() {
--- src/main/java/org/zerock/controller/RestControllerEx2.java
+++ src/main/java/org/zerock/controller/RestControllerEx2.java
@@ -1,8 +1,11 @@
 package org.zerock.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.Rest1;
@@ -76,13 +79,68 @@ public class RestControllerEx2 {
 		return r2;
 	}
 	
-	@RequestMapping("/ex999")
-	public List<Integer> method999() {
-		List<Integer> list = new ArrayList<>();
-		for (int i = 0; i < 5; i++) {
-			 list.add((int) (Math.random() * 100));
-		}
+	@RequestMapping("/ex6")
+	public String[] method6() {
+		String[] arr = {"java", "json", "xml"};
+		
+		return arr;
+	}
+	
+	@RequestMapping("/ex7")
+	public List<String> method7() {
+		List<String> list = new ArrayList<>();
+		list.add("hello");
+		list.add("world");
+		list.add("spring");
+		
 		return list;
+	}
+	
+	@RequestMapping("/ex8")
+	public Map<String, String> method8() {
+		Map<String, String> map = new HashMap<>();
+		map.put("java", "script");
+		map.put("hello", "world");
+		map.put("spring", "boot");
+		
+		return map;
+	}
+	
+	@RequestMapping("/ex9")
+	public List<Rest1> method9() {
+		List<Rest1> list = new ArrayList<>();
+		
+		Rest1 r1 = new Rest1();
+		r1.setName("trump");
+		r1.setAge(33);
+		r1.setVote(true);
+		
+		list.add(r1);
+		
+		Rest1 r2 = new Rest1();
+		r2.setName("한글");
+		r2.setAge(22);
+		r2.setVote(false);
+		
+		list.add(r2);
+		
+		
+		return list;
+	}
+	
+	// 특정 status code로 응답할 때
+	@RequestMapping("/ex10")
+	public ResponseEntity<String> method10() {
+		return ResponseEntity.status(200).body("hello");
+	}
+	
+	@RequestMapping("/ex11")
+	public ResponseEntity<String> method11(int num) {
+		if (num > 0) {
+			return ResponseEntity.status(200).body("spring");
+		} else {
+			return ResponseEntity.status(404).body("");
+		}
 	}
 }
 
