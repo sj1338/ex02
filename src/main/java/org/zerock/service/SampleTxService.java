1d799e0437a5273d97844394bb368105510e2ce6
id177 <id177@DESKTOP-OV3556V>
2021-02-01 09:49:07 +0900

    a

---
+++ src/main/java/org/zerock/service/SampleTxService.java
@@ -0,0 +1,33 @@
+package org.zerock.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.mapper.Sample1Mapper;
+import org.zerock.mapper.Sample2Mapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+public class SampleTxService {
+	
+	@Setter(onMethod_ = @Autowired)
+	private Sample1Mapper mapper1;
+	@Setter(onMethod_ = @Autowired)
+	private Sample2Mapper mapper2;
+	
+	@Transactional
+	public void addData(String value) {
+		mapper1.insertCol1(value); // 500 bytes ok
+		mapper2.insertCol2(value); // 50 bytes fail
+	}
+	
+}
+
+
+
+
+
+
